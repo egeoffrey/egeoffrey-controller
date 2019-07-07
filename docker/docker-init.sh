57e98823991a6ba914734251b693fa970c92f560
user2684 <user2684@users.noreply.github.com>
2019-07-07 15:40:09 +0200

    Updated docker init

--- docker/docker-init.sh
+++ docker/docker-init.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 CONFIG_MOUNT="/config"
+LOGS_MOUNT="/logs"
 
 # create the config directory
 mkdir -p $CONFIG_MOUNT
@@ -11,4 +12,10 @@ if [ ! "$(ls -A $CONFIG_MOUNT)" ]; then
 fi
 # make a symbolic link so to use the user's configuration
 rm -rf config
-ln -s $CONFIG_MOUNT $CONFIG
+ln -s $CONFIG_MOUNT ./config
+
+# create the logs directory
+mkdir -p $LOGS_MOUNT
+# make a symbolic link
+rm -rf logs
+ln -s $LOGS_MOUNT ./logs
