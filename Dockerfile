90811af63c91fa457af81e25ce65796024966103
user2684 <user2684@users.noreply.github.com>
2019-08-27 19:11:15 +0200

    Re-added controller/config back

--- Dockerfile
+++ Dockerfile
@@ -5,9 +5,6 @@ ARG SDK_VERSION
 ARG ARCHITECTURE
 FROM egeoffrey/egeoffrey-sdk-alpine:${SDK_VERSION}-${ARCHITECTURE}
 
-### disable local logging since running logger service
-ENV EGEOFFREY_LOGGING_LOCAL=0
-
 ### install module's dependencies
 RUN pip install fuzzywuzzy apscheduler redis==2.10.6 rq==0.12.0
 
--- docker/docker-init.sh
+++ docker/docker-init.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
+CONFIG_MOUNT="/config"
 LOGS_MOUNT="/logs"
+
+# create the config directory
+mkdir -p $CONFIG_MOUNT
+# if the directory is empty copy there the default configuration
+if [ ! "$(ls -A $CONFIG_MOUNT)" ]; then
+    echo -e "Copying default configuration into $CONFIG_MOUNT..."
+    cp -Rf config/* $CONFIG_MOUNT
+fi
+# make a symbolic link so to use the user's configuration
+rm -rf config
+ln -s $CONFIG_MOUNT ./config
 
 # create the logs directory
 mkdir -p $LOGS_MOUNT
